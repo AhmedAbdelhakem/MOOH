442b2eaf63cc0f477ceca8bde8ab220ad927230c
Ahmed Abdelhakeem <100963499+AhmedAbdelhakem@users.noreply.github.com>
2025-05-21 12:19:43 +0300

    First commit

---
+++ android/app/src/main/kotlin/com/example/mooh/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mooh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+            Text("data"),
+          ],
+        ),
+      ),
+    );
+  }
+}
